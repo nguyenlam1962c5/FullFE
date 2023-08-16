f7e045d0ff6e32bd300b305061b0caf98799e2e2
nguyenlam1962c5 <nguyenlam1962c5@gmail.com>
2023-08-16 14:30:45 +0700

    commit1

--- src/contracts/utils/getAddress.ts
+++ src/contracts/utils/getAddress.ts
@@ -6,8 +6,8 @@ const getAddress = (address: AddressType) => {
 };
 
 export const getCrowdSaleAddress =()=> getAddress(SMART_ADDRESS.CROWDSALE);
-export const getUSDTAddress =()=> getAddress(SMART_ADDRESS.USDT);
 export const getNFTCardAddress =()=> getAddress(SMART_ADDRESS.NFTCARD);
 export const getNFTCardMarketplacetAddress =()=> getAddress(SMART_ADDRESS.NFTCARDMARKETPLACE);
 export const getAuctionAddress =()=> getAddress(SMART_ADDRESS.AUCTION);
 export const getBNBTAddress =()=> getAddress(SMART_ADDRESS.BNBT);
+export const getUSDTAddress =()=> getAddress(SMART_ADDRESS.USDT);
